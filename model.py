707c3eecbe15793c77dc6ae1785c3f8734387dcd
donghyeon_kim <donghyeon@korea.ac.kr>
2018-08-11 12:22:12 +0900

    Chore

---
+++ model.py
@@ -0,0 +1,16 @@
+# import torch
+# import torch.nn.functional as F
+from torch import nn
+
+
+class MatchLSTM(nn.Module):
+    def __init__(self, config):
+        super(MatchLSTM, self).__init__()
+
+        # self.attention = nn.Linear(in_features=, out_features=)
+
+        # self.lstm_t = nn.LSTM()
+        # self.lstm_m = nn.LSTM()
+
+    def forward(self, x):
+        pass
